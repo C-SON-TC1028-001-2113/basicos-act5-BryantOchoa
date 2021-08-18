6cc3e2b304d0d31b9063765eea0d2d03197c2243
BryantOchoa <A01252419@itesm.mx>
2021-08-18 04:28:23 +0000

    act13bryant

--- assignments/13CantidadLitrosPintura/src/exercise.py
+++ assignments/13CantidadLitrosPintura/src/exercise.py
@@ -1,6 +1,11 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
+    area = float(input('Area a pintar en metros:  '))
+    rendimiento = float(input("Rendimiento (m2/l)"))
+    Litros = math.ceil(area/rendimiento)
+
+    print('Litros a comprar: '+str (Litros))
 
 
 if __name__ == '__main__':
--- assignments/15Escalera/src/exercise.py
+++ assignments/15Escalera/src/exercise.py
@@ -1,6 +1,11 @@
 import math
 def main():
-    #escribe tu código abajo de esta línea
+#Escribe tu código abajo de esta línea 
+
+    altura = float(input('Altura de la casa: ' ))
+    angulo = float(input('Angulo: '))
+    largo = math.ceil(altura/math.sin(angulo))
+    print('Lo largo de la escalera es: '+ str(largo))
 
 if __name__ == '__main__':
     main()
